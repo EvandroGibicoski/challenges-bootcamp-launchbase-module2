0faa83f63613ae92c0339662c1e3d082363eb4a1
Evandro Gibicoski <evandrogibicoski@gmail.com>
2020-04-30 01:13:16 -0300

    working in the project

--- routes.js
+++ routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const routes =  express.Router();
+const teachers = require('./teachers'); 
 
-routes.get("/", function(require, response) {
-    return response.render("teachers/index");
+routes.get("/teachers", function(req, res) {
+    return res.render("teachers/index");
 });
 
-routes.get("/students", function(require, response) {
-    return response.render("students");
+routes.get("/teachers/create", function(req, res) {
+    return res.render("teachers/create")
 });
+
+routes.post("/teachers", teachers.post)
+
+routes.get("/students", function(req, res) {
+    return res.render("students");
+});
+
 
 module.exports = routes;
