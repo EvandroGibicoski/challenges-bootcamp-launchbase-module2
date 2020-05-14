f8c42d861bd07cdd326fe62c15ad83e69eb7e2a8
EvandroGibicoski <evandrogibicoski@outlook.com>
2020-05-13 23:15:37 -0300

    fix: return degree  to frontend ok

--- utils.js
+++ utils.js
@@ -1,15 +1,25 @@
+
 module.exports = {
     age: function(timestamp){
         const today = new Date()
-        const birthDay = new Date(timestamp);
+        const birthDate = new Date(timestamp);
 
-    let age = today.getFullYear() - birthDay.getFullYear();
+    let age = today.getFullYear() - birthDate.getFullYear();
 
-        const month = today.getMonth() - birthDay.getMonth();
-            if(month < 0 || month == 0 && today.getDate() <= birthDay.getDate()) {
+        const month = today.getMonth() - birthDate.getMonth();
+            if(month < 0 || month == 0 && today.getDate() <= birthDate.getDate()) {
                 age = age -1
             }
 
             return age
+    },
+    graduation: function degree (value) {
+            switch(value) {
+                case("medio"): return "Ensino Médio Completo";
+                case("superior"): return "Ensino Supeior Completo";
+                case("mestre"): return "Mestrado";
+                case("doutor"): return "Doutorado";
+        }
     }
 }
+
